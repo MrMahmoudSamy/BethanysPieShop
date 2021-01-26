9c91030e3cb35afcc1b236d7f94c91b482622d7e
Mahmoud Samy <mahmodsamy102@gmail.com>
2021-01-26 11:20:58 +0200

    Create Controller

---
+++ BethanysPieShop/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanysPieShop.Controllers
+{
+    public class ProductController : Controller
+    {
+        private ICategoryRepository _categoryRepository;
+        private IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository,ICategoryRepository categoryRepository)
+        {
+            _categoryRepository = categoryRepository;
+            _productRepository = productRepository;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public ViewResult List()
+        {
+            return View(_productRepository.Allproducts);
+        }
+    }
+}
