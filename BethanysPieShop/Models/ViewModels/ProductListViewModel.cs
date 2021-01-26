706fcc544517db601120bc9c3a70a7df56512d33
Mahmoud Samy <mahmodsamy102@gmail.com>
2021-01-26 13:50:26 +0200

    Create Views and Viewmodel

---
+++ BethanysPieShop/Models/ViewModels/ProductListViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Models.ViewModels
+{
+    public class ProductListViewModel
+    {
+        public IEnumerable<Product> ProductsList { get; set; }
+        public string CurrentCategory { get; set; }
+    }
+}
