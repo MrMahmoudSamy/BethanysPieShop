20b38b652b8dc460d53ca7e185271b6cddb444cf
Mahmoud Samy <mahmodsamy102@gmail.com>
2021-02-08 15:35:48 +0200

    Creating the Real Repository

---
+++ BethanysPieShop/Repositories/CategorySqlRepository.cs
@@ -0,0 +1,19 @@
+﻿using BethanysPieShop.Data;
+using BethanysPieShop.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BethanysPieShop.Repositories
+{
+    public class CategorySqlRepository:ICategoryRepository
+    {
+        private AppDbContext _context;
+
+        public CategorySqlRepository(AppDbContext Context)
+        {
+            _context = Context;
+        }
+
+        public IEnumerable<Category> Categories => _context.Categories.ToList();
+    }
+}
---
+++ BethanysPieShop/Repositories/ProductSqlRepository.cs
@@ -0,0 +1,38 @@
+﻿using BethanysPieShop.Data;
+using BethanysPieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieShop.Repositories
+{
+    public class ProductSqlRepository : IProductRepository
+    {
+        private AppDbContext _context;
+
+        public ProductSqlRepository(AppDbContext Context) 
+        {
+            _context = Context;
+        }
+        public IEnumerable<Product> Allproducts
+        {
+            get
+            {
+                return _context.Products.Include(c=>c.Category).ToList();
+            }
+        }
+        public IEnumerable<Product> AllproductOftheWeek
+        {
+            get
+            {
+                return _context.Products.Include(c => c.Category).Where(p=>p.IsProductOftheWeek);
+            }
+        }
+
+        public Product GetProductById(int prodId)
+        {
+            return _context.Products.SingleOrDefault(p=>p.ProductId==prodId);
+        }
+    }
+}
