760cf3510787527f85104d2765f57ce3fa19e8e6
Mahmoud Samy <mahmodsamy102@gmail.com>
2021-02-15 15:07:43 +0200

    Seeding Data

--- BethanysPieShop/Data/AppDbContext.cs
+++ BethanysPieShop/Data/AppDbContext.cs
@@ -15,5 +15,21 @@ namespace BethanysPieShop.Data
         }
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
+
+        protected override void OnModelCreating  (ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(new Category { CategoryId = 1, CategoryName = "Fruit Pie", Description = "All fruit pie" },
+               new Category { CategoryId = 2, CategoryName = "Cheese cakes", Description = "Chees" },
+               new Category { CategoryId = 3, CategoryName = "Coffee", Description = "Coffee beans makes drinks" },
+               new Category { CategoryId = 4, CategoryName = "Fruit Juice", Description = "All fruit juice" });
+
+            modelBuilder.Entity<Product>().HasData(new Product { CategoryId = 1, ProductId = 1, Name = "Strawberry Pie", Price = 15.95M, ImageThumbnailUrl = "~/images/59b13b33-a1fd-47b3-a376-f735af81c40a.jpg" },
+            new Product { CategoryId = 1, ProductId = 2, Name = "Appel Pie", Price = 15.95M, ImageThumbnailUrl = "~/images/ApplePie.jpg" },
+            new Product { CategoryId = 2, ProductId = 3, Name = "Cheese cake", Price = 15.95M, ImageThumbnailUrl = "~/images/CheeseCacke.jpg" },
+            new Product { CategoryId = 3, ProductId = 4, Name = "Turkey coffe", Price = 15.95M, ImageThumbnailUrl = "~/images/turkish-coffee.jpg" },
+            new Product { CategoryId = 4, ProductId = 5, Name = "Strawberry Smoothy", Price = 15.95M, ImageThumbnailUrl = "~/images/strawberrysmoothie.jpg" });
+
+        }
     }
 }
---
+++ BethanysPieShop/Migrations/20210215121156_SeedData.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BethanysPieShop.Migrations
+{
+    public partial class SeedData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "CategoryId", "CategoryName", "Description" },
+                values: new object[,]
+                {
+                    { 1, "Fruit Pie", "All fruit pie" },
+                    { 2, "Cheese cakes", "Chees" },
+                    { 3, "Coffee", "Coffee beans makes drinks" },
+                    { 4, "Fruit Juice", "All fruit juice" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "ProductId", "CategoryId", "ImageThumbnailUrl", "ImageUrl", "InStock", "IsProductOftheWeek", "LongDescription", "Name", "Price", "ShortDescription" },
+                values: new object[,]
+                {
+                    { 1, 1, "~/images/59b13b33-a1fd-47b3-a376-f735af81c40a.jpg", null, false, false, null, "Strawberry Pie", 15.95m, null },
+                    { 2, 1, "~/images/ApplePie.jpg", null, false, false, null, "Appel Pie", 15.95m, null },
+                    { 3, 2, "~/images/CheeseCacke.jpg", null, false, false, null, "Cheese cake", 15.95m, null },
+                    { 4, 3, "~/images/turkish-coffee.jpg", null, false, false, null, "Turkey coffe", 15.95m, null },
+                    { 5, 4, "~/images/strawberrysmoothie.jpg", null, false, false, null, "Strawberry Smoothy", 15.95m, null }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "ProductId",
+                keyValue: 5);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "CategoryId",
+                keyValue: 4);
+        }
+    }
+}
